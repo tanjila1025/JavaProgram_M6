a1c15d91347b3dc1c0e9ebd6b47bca59a8ec9f34
Tanjila Attar <158995214+tanjila25@users.noreply.github.com>
2024-02-09 16:16:34 +0530

    Add files via upload

---
+++ Arethmatic_Operator_Pgm.java
@@ -0,0 +1,31 @@
+class Arethmatic_Operator_Pgm{
+public static void main(String []args){
+ int a=100;
+ int b=50;
+
+ int c=a+b;//addition of two operands.
+ System.out.println(c);
+
+ int d=a*b;//multiplication of two operands.
+ System.out.println(d);
+ 
+ int f=a-b;//substraction of two operands.
+ System.out.println(f);
+
+ int g=a/b;//division of two operands..division always return quotient.
+ System.out.println(g);
+
+ float k=a%b;//modulus of two operand..modulus always return reminder. 
+ System.out.println(k);
+
+ }
+}
+/*Output:
+C.T:success
+R.T:success:150
+            5000
+            50
+            2
+            0.0
+*/
+
