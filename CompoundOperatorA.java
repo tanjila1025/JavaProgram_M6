e62e595d2887522cc304b7eb7705b3d58b70609c
Tanjila Attar <158995214+tanjila25@users.noreply.github.com>
2024-02-15 16:11:31 +0530

    Add files via upload

---
+++ CompoundOperatorA.java
@@ -0,0 +1,27 @@
+class CompoundOperatorA{
+   
+   public static void main(String[] args){  
+    int a=50 ,a1=40,a2=60,a3=90,a4=80;
+    a+=2;
+    a1-=4;
+    a2*=5;
+    a3/=2;
+    a4%=2;
+    System.out.println(a);
+    System.out.println(a1);
+    System.out.println(a2);
+    System.out.println(a3);
+    System.out.println(a4);
+
+  }
+
+} /*OUTPUT:C.T=success
+           R.T=Success
+           52 
+           36 
+           300 
+           45 
+           0
+   */
+           
+    
