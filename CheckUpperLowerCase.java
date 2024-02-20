205cb6efee036a410a2d40014c5e95893ba9d5ad
Tanjila Attar <158995214+tanjila25@users.noreply.github.com>
2024-02-20 19:39:52 +0530

    Update CheckUpperLowerCase.java

--- CheckUpperLowerCase.java
+++ CheckUpperLowerCase.java
@@ -3,10 +3,14 @@ class CheckUpperLowerCase{
     public static void main(String[] args){
        
      char c1='A';//variable decleration &  initilization
-     String result=(c1>=65&&c1<=90)?("given char is uppercase"):(" the given char is lower case");//using ternery operator 
+     String result=(c1>=65&&c1<=90)?("given char is in uppercase"):(" the given char is in lower case");//using ternery operator 
      System.out.println(result);
     }
 }
+/*OUTPUT:
+ C.T:success
+ R.T:success:the given char is in uppercase
+ */
 
     
     
