ed15f111e9e7f4ecdf15bfb27503563ca73ab9ed
Tanjila Attar <158995214+tanjila1025@users.noreply.github.com>
2024-03-14 14:44:43 +0530

    Add files via upload

---
+++ Loop_Example.java
@@ -0,0 +1,24 @@
+class Loop_Example{
+   
+   public static void main(String[] args){
+    
+     int v=1;
+     
+     do{
+     
+       System.out.println(v);
+        v++;
+      }while(v<=10);
+   }
+}
+/*OUTPUT:1
+2
+3
+4
+5
+6
+7
+8
+9
+10
+*/
---
+++ Loop_Example1.java
@@ -0,0 +1,25 @@
+class Loop_Example1{
+   
+   public static void main(String[] args){
+   
+   int v=1;
+   do{
+      System.out.println(2*v);
+      v++;
+      }while(v<=10);
+    }
+}
+/*
+  OUTPUT:
+2
+4
+6
+8
+10
+12
+14
+16
+18
+20
+*/
+         
